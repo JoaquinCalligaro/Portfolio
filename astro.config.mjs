0ff2a9af478d7e2646551806138f213a41d9efc6
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-22 20:20:52 -0300

    Refactor Card and CardService components: move to Cards directory and update imports. Remove old component files and create new ones with the same functionality.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // 👈 renderizado del servidor
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }), // 👈 adaptador de Vercel con configuración
   vite: {
     plugins: [tailwindcss()],
   },
